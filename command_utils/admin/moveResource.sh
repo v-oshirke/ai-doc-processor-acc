3caf1b50bad8fb3cc22050069f7c656a212af01d
Mark Remmey <mremmey7@gmail.com>
2025-01-28 10:48:29 -0500

    Updating bicep template with static web app, removing unecessary templates

--- command_utils/admin/moveResource.sh
+++ command_utils/admin/moveResource.sh
@@ -1,1 +1,1 @@
-az resource move --destination-group llm-1-24 --ids /subscriptions/253f778b-553f-4871-b143-123f314c45c1/resourceGroups/static-web-app_group/providers/Microsoft.Web/staticSites/static-web-app
+az resource move --destination-group  --ids 
